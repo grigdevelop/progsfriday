6f299504c26bff09d4de7e5e345513cbc87fb91e
Grigor <grigdevelop@gmail.com>
2018-02-19 10:37:56 +0100

    test fast

--- ProgsFriday.Cons/Program.cs
+++ ProgsFriday.Cons/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using ProgsFriday.Lib.Algorithms;
 
 namespace ProgsFriday.Cons
@@ -9,11 +10,30 @@ namespace ProgsFriday.Cons
     {
         static void Main(string[] args)
         {
-            int[] list = new int[] { 5, 4, 3, 2, 1, 11, 23, 11, 12, 22, 3, 1, 2, 3 };
-            list.MergeSort();
+            Random rd = new Random();
+            int[] list = Enumerable.Range(1, 2228000).OrderBy(x => rd.Next()).ToArray();
 
-            Console.WriteLine(string.Join(",", list));
+            CalcTime(() =>
+            {
+                list.OrderBy(x => x);
+            });
+
+            CalcTime(() =>
+            {
+                list.QuickSort();
+            });
+
             Console.ReadLine();
+        }
+
+        static void CalcTime(Action action)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            action();
+            sw.Stop();
+            var t = new TimeSpan(sw.ElapsedTicks);
+            Console.WriteLine("Spend time: {0}", t.ToString());
         }
     }
 }
